ed81b2784de68b87f7842063219263276bb26b15
sushant81074 <sushant94601@gmail.com>
2024-06-18 14:13:54 +0530

    usermodel

---
+++ makemytrip/src/model/user.model.ts
@@ -0,0 +1,47 @@
+import mongoose, { Document } from "mongoose";
+
+export interface UserInterface extends Document {
+  username: string;
+  email: string;
+  password: string;
+  contactNo: number;
+  isVerified: boolean;
+  isActive: boolean;
+}
+
+const userSchema: mongoose.Schema<UserInterface> = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      trim: true,
+      match: [/.+\@.+\..+/, "please insert a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    contactNo: {
+      type: Number,
+      required: [true, "contact no. is required"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model<UserInterface>("User", userSchema);
+
+export default User;
