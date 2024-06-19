1237b717b8a007c22a2f4271889365151c9ec9db
sushant81074 <sushant94601@gmail.com>
2024-06-19 19:02:38 +0530

    sign-up and verify account bugs resolved

--- makemytrip/src/utils/nodemailer.js
+++ makemytrip/src/utils/nodemailer.js
@@ -2,7 +2,7 @@ import nodemailer, { Transporter } from "nodemailer";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || "smtp.example.com",
+  host: process.env.EMAIL_HOST || "smtp.gmail.com",
   port: process.env.EMAIL_PORT || 587,
   secure: false, // Adjust based on your SMTP server configuration
   auth: {
